f74ffc880629f54eb3a21e18e6ddf946f89fc820
Krzysztof Kula <krzychukula@gmail.com>
2014-09-01 15:24:24 +0200

    page 239, code for drawing lines

--- TouchTracker/BNRDrawView.m
+++ TouchTracker/BNRDrawView.m
@@ -9,6 +9,49 @@
 #import "BNRDrawView.h"
 #import "BNRLine.h"
 
+@interface BNRDrawView ()
+
+@property (nonatomic, strong) BNRLine *currentLine;
+@property (nonatomic, strong) NSMutableArray *finishedLines;
+
+@end
+
 @implementation BNRDrawView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if(self){
+        self.finishedLines = [[NSMutableArray alloc] init];
+        self.backgroundColor = [UIColor grayColor];
+    }
+    return self;
+}
+
+- (void)strokeLine:(BNRLine *)line
+{
+    UIBezierPath *bp = [UIBezierPath bezierPath];
+    bp.lineWidth = 10;
+    bp.lineCapStyle = kCGLineCapRound;
+    
+    [bp moveToPoint:line.begin];
+    [bp addLineToPoint:line.end];
+    [bp stroke];
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    //draw finished lines in black
+    [[UIColor blackColor] set];
+    for (BNRLine *line in self.finishedLines) {
+        [self strokeLine:line];
+    }
+    
+    if (self.currentLine) {
+        //if there is a line currently being drawn, do it in red
+        [[UIColor redColor] set];
+        [self strokeLine:self.currentLine];
+    }
+}
 
 @end
