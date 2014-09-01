60c7a7f681de29ee975803ac713da0ccb9ccc50d
Krzysztof Kula <krzychukula@gmail.com>
2014-09-01 14:10:57 +0200

    init project

---
+++ TouchTracker/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  TouchTracker
+//
+//  Created by Krzysztof Kula on 01.09.2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TouchTrackerTests/TouchTrackerTests.m
@@ -0,0 +1,34 @@
+//
+//  TouchTrackerTests.m
+//  TouchTrackerTests
+//
+//  Created by Krzysztof Kula on 01.09.2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TouchTrackerTests : XCTestCase
+
+@end
+
+@implementation TouchTrackerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
