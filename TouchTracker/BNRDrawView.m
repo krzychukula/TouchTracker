fea0a5f000444074d6ade545ed554402a0e6a269
krzychukula <krzychukula@gmail.com>
2014-10-06 07:19:17 +0200

    page 254. selecting a line

--- TouchTracker/BNRDrawView.m
+++ TouchTracker/BNRDrawView.m
@@ -14,6 +14,8 @@
 @property (nonatomic, strong) NSMutableDictionary *linesInProgress;
 @property (nonatomic, strong) NSMutableArray *finishedLines;
 
+@property (nonatomic, weak) BNRLine *selectedLine;
+
 @end
 
 @implementation BNRDrawView
@@ -63,6 +65,11 @@
     [[UIColor redColor] set];
     for (NSValue *key in self.linesInProgress) {
         [self strokeLine:self.linesInProgress[key]];
+    }
+    
+    if (self.selectedLine) {
+        [[UIColor greenColor] set];
+        [self strokeLine:self.selectedLine];
     }
 }
 
@@ -124,9 +131,37 @@
     [self setNeedsDisplay];
 }
 
+- (BNRLine *)lineAtPoint:(CGPoint)p
+{
+    //find a line close to p
+    for (BNRLine *l in self.finishedLines) {
+        CGPoint start = l.begin;
+        CGPoint end   = l.end;
+        
+        //check a few points on the line
+        for (float t = 0.0; t <= 1.0; t += 0.05) {
+            float x = start.x + t * (end.x - start.x);
+            float y = start.y + t * (end.y - start.y);
+            
+            //if a tapped point is within 20 points, let's return this line
+            if (hypot(x - p.x, y - p.y) < 20) {
+                return l;
+            }
+        }
+    }
+    
+    //if nothing is close enough to the tapped point, then we did not select a line
+    return nil;
+}
+
 - (void)tap:(UITapGestureRecognizer *)gr
 {
     NSLog(@"Recognized tap");
+    
+    CGPoint point = [gr locationInView:self];
+    self.selectedLine = [self lineAtPoint:point];
+    
+    [self setNeedsDisplay];
 }
 
 
